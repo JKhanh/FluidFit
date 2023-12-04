4f0dd2461cbeaa3bf04a9fc5ea125564854260a2
Nguyen Quoc Khanh <nguyenkhanhcv@gmail.com>
2023-12-04 21:00:11 +0700

    Initial commit

---
+++ composeApp/src/androidMain/kotlin/org/jkhanh/fluidfit/FluidFitApplication.kt
@@ -0,0 +1,12 @@
+package org.jkhanh.fluidfit
+
+import android.app.Application
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
+
+class FluidFitApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Napier.base(DebugAntilog())
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/NapierSetup.kt
@@ -0,0 +1,6 @@
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
+
+fun napierInit() {
+    Napier.base(DebugAntilog())
+}
